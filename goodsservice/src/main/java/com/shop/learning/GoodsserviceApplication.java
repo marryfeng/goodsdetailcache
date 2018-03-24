839be753652767ebbc89c7a0cc0e58c5ac225acb
imooc <1473357859@qq.com>
2018-03-24 11:09:35 +0800

    商品服务修改

--- goodsservice/src/main/java/com/shop/learning/GoodsserviceApplication.java
+++ goodsservice/src/main/java/com/shop/learning/GoodsserviceApplication.java
@@ -1,11 +1,14 @@
 package com.shop.learning;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableEurekaClient
+@ComponentScan
 public class GoodsserviceApplication {
 
 	public static void main(String[] args) {
