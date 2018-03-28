c5f3448f3a2ae83416ddb5b67baf1faca798e31e
imooc <1473357859@qq.com>
2018-03-28 20:47:17 +0800

    eshop的商品服务的普通增删改查

--- stockservice00/src/main/java/com/shop/learning/Stockservice00Application.java
+++ stockservice00/src/main/java/com/shop/learning/Stockservice00Application.java
@@ -3,7 +3,12 @@ package com.shop.learning;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -11,6 +16,24 @@ import org.springframework.context.annotation.ComponentScan;
 public class Stockservice00Application {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(Stockservice00Application.class, args);
 	}
+
+	@Bean
+	public JedisPool jedisPool(){
+		JedisPoolConfig config=new JedisPoolConfig();
+		config.setMaxIdle(100);
+		config.setMaxTotal(1024);
+		config.setMaxWaitMillis(100);
+		config.setTestOnBorrow(true);
+		return new JedisPool(config,"localhost",6379);
+
+	}
+
+
+
+
+
+
 }
